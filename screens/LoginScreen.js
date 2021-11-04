1ee91cf38f3b6c1ff4eb2dfd12bab95afa0b7707
Cole Spector <cgs26@duke.edu>
2021-11-03 21:58:26 -0400

    basic login ui

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,14 +1,38 @@
 import React from 'react'
-import { StyleSheet, Text, View, KeyboardAvoidingView } from 'react-native'
+import { StyleSheet, Text, View, KeyboardAvoidingView, TextInput, TouchableOpacity } from 'react-native'
 
 const LoginScreen = () => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
     return (
         <KeyboardAvoidingView
             style={styles.container}
             behavior="padding">
 
-        <View>
+        <View style={styles.inputContainer}>
+            <TextInput 
+                placeholder="Email"
+                value = {email}
+                onChangeText={ text => setEmail(text) }
+                stule={styles.input}
+            />
+            <TextInput 
+                placeholder="Password"
+                value = {password}
+                onChangeText={ text => setPassword(text)}
+                style={styles.input}
+                secureTextEntry
+            />
             <Text>Login Screen</Text>
+        </View>
+        <View style={styles.buttonContainer}>
+            <TouchableOpacity
+                onPress={() => {}}
+                style={[styles.button, styles.buttonOutline]}
+            >
+                <Text style={styles.buttonOutlineText}>Login</Text>
+
+            </TouchableOpacity>
         </View>
 
         </KeyboardAvoidingView>
@@ -17,4 +41,10 @@ const LoginScreen = () => {
 
 export default LoginScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container : {
+      justifyContent: 'center',
+      alignItems : 'center',
+      flex: 1,
+    }
+})
