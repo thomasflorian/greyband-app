b5769947964e033abcb5a44bfc481ff7c88c1db0
Cole Spector <cgs26@duke.edu>
2021-11-05 14:58:55 -0400

    broke something

--- App.js
+++ App.js
@@ -21,30 +21,30 @@ export default function App() {
   
   const Stack = createNativeStackNavigator();
 
-    // Load Montserrat font
-    let [fontsLoaded] = useFonts({
-      Montserrat_500Medium,
-      Montserrat_400Regular,
-      Montserrat_700Bold
-    });
+  // Load Montserrat font
+  let [fontsLoaded] = useFonts({
+    Montserrat_500Medium,
+    Montserrat_400Regular,
+    Montserrat_700Bold
+  });
 
 
-  const theme = {
-    dark: true,
-    colors: {
-      primary: '#D64F27',
-      background: '#0F2138',
-      card: '#0F2138',
-      text: '#D64F27',
-      border: '#D64F27',
-      notification: '#D64F27',
-    },
-    font: {
-      bold: "Montserrat_700Bold",
-      light: "Montserrat_400Regular",
-      regular: "Montserrat_500Medium",
-    }
-  };
+const theme = {
+  dark: true,
+  colors: {
+    primary: '#D64F27',
+    background: '#0F2138',
+    card: '#0F2138',
+    text: '#D64F27',
+    border: '#D64F27',
+    notification: '#D64F27',
+  },
+  font: {
+    bold: "Montserrat_700Bold",
+    light: "Montserrat_400Regular",
+    regular: "Montserrat_500Medium",
+  }
+};
 
   useEffect(() => {
     auth.onAuthStateChanged(user => {
@@ -58,31 +58,98 @@ export default function App() {
     })
   })  
 
-  // if(isLoggedIn){
-  //   return (
-  //     !fontsLoaded ? <AppLoading /> :
-  //      <View style={styles(theme).container}>
-  //      <NavigationContainer theme={theme}>
-  //        <Menubar />
-  //        <Stack.Navigator initialRouteName="Home" screenOptions={{headerShown: false, gestureEnabled: false, animation: "none"}} >
-  //          <Stack.Screen name="Search" component={SearchScreen} />
-  //          <Stack.Screen name="Home" component={HomeScreen} />
-  //          <Stack.Screen name="Party" component={PartyScreen} />
-  //          <Stack.Screen name="Profile" component={ProfileScreen} />
-  //          <Stack.Screen name="EditProfile" component={EditProfileScreen} />
-  //          <Stack.Screen name="CreateParty" component={CreatePartyScreen} />
-  //        </Stack.Navigator>
-  //      </NavigationContainer>
-  //      <StatusBar style="auto" />
-  //    </View>
-  //  );
-  // } 
-  // return (
-  //   <LoginScreen />
-  // )
-  return (<IntroScreen />)
+  //if(isLoggedIn){
+    return (
+      !fontsLoaded ? <AppLoading /> :
+      <View style={styles(theme).container}>
+      <NavigationContainer theme={theme}>
+        <Menubar />
+        <Stack.Navigator initialRouteName="Home" screenOptions={{headerShown: false, gestureEnabled: false, animation: "none"}} >
+          <Stack.Screen name="Search" component={SearchScreen} />
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Party" component={PartyScreen} />
+          <Stack.Screen name="Profile" component={ProfileScreen} />
+          <Stack.Screen name="EditProfile" component={EditProfileScreen} />
+          <Stack.Screen name="CreateParty" component={CreatePartyScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+      <StatusBar style="auto" />
+    </View>
+    );
+    
+  //} 
+  //return (
+    //<LoggedOutPath />
+  //);
+   
   
+    
+
+
 }
+
+// const LoggedInPath = ( {navigation }) => {
+
+//   const Stack = createNativeStackNavigator();
+
+//     // Load Montserrat font
+//     let [fontsLoaded] = useFonts({
+//       Montserrat_500Medium,
+//       Montserrat_400Regular,
+//       Montserrat_700Bold
+//     });
+
+
+//   const theme = {
+//     dark: true,
+//     colors: {
+//       primary: '#D64F27',
+//       background: '#0F2138',
+//       card: '#0F2138',
+//       text: '#D64F27',
+//       border: '#D64F27',
+//       notification: '#D64F27',
+//     },
+//     font: {
+//       bold: "Montserrat_700Bold",
+//       light: "Montserrat_400Regular",
+//       regular: "Montserrat_500Medium",
+//     }
+//   };
+
+
+//   return (
+//      !fontsLoaded ? <AppLoading /> :
+//       <View style={styles(theme).container}>
+//       <NavigationContainer theme={theme}>
+//         <Menubar />
+//         <Stack.Navigator initialRouteName="Home" screenOptions={{headerShown: false, gestureEnabled: false, animation: "none"}} >
+//           <Stack.Screen name="Search" component={SearchScreen} />
+//           <Stack.Screen name="Home" component={HomeScreen} />
+//           <Stack.Screen name="Party" component={PartyScreen} />
+//           <Stack.Screen name="Profile" component={ProfileScreen} />
+//           <Stack.Screen name="EditProfile" component={EditProfileScreen} />
+//           <Stack.Screen name="CreateParty" component={CreatePartyScreen} />
+//         </Stack.Navigator>
+//       </NavigationContainer>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
+
+// const LoggedOutPath = ( {navigation}) => {
+
+//   const Stack2 = createNativeStackNavigator();
+
+//   return (
+//     <NavigationContainer>
+//     <Stack2.Navigator initialRouteName="Intro" screenOptions={{headerShown: false}}>
+//       <Stack2.Screen name="Intro" component={IntroScreen} />
+//       <Stack2.Screen name="Login" component={LoginScreen} />
+//     </Stack2.Navigator>
+//   </NavigationContainer>
+//   ) 
+
 
 const styles = theme => StyleSheet.create({
   container: {
@@ -92,3 +159,4 @@ const styles = theme => StyleSheet.create({
     paddingTop: 30
   },
 });
+
