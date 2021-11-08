297b415b5e417b6a0065452cf84c5db07ad32e39
Cole Spector <cgs26@duke.edu>
2021-11-07 21:11:47 -0500

    creased base CreateAccountScreen

---
+++ screens/login/CreateAccounttScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const CreateAccounttScreen = () => {
+    return (
+        <View>
+            <Text></Text>
+        </View>
+    )
+}
+
+export default CreateAccounttScreen
+
+const styles = StyleSheet.create({})
