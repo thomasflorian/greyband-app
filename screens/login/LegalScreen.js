03e7b49c5930b1e413d1681c0d7637323551125f
Cole Spector <cgs26@duke.edu>
2021-11-21 15:19:03 -0500

    legal screen startup base

--- screens/login/LegalScreen.js
+++ screens/login/LegalScreen.js
@@ -1,11 +1,14 @@
 import React, { useState } from 'react'
-import { StyleSheet, Text, View, Button, SafeAreaView, TouchableOpacity, Pressable } from 'react-native'
+import { StyleSheet, Text, View, Button, SafeAreaView, TouchableOpacity, Pressable, Dimensions } from 'react-native'
 import { useTheme } from '@react-navigation/native';
+
+const WIDTH = Dimensions.get('window').width
 
 const LegalScreen = ({navigation}, props) => {
     const theme = useTheme();
-    const [buttonColor, setButtonColor] = useState(theme.colors.background)
-    const [isButtonActive, setIsButtonActive] = useState(false);
+    const [isAgeButtonActive, setIsAgeButtonActive] = useState(false);
+    const [isLegalButtonActive, setIsLegalButtonActive] = useState(false);
+    
 
     const changeButtonColor = () => {
         if (isButtonActive) {
@@ -34,22 +37,17 @@ const LegalScreen = ({navigation}, props) => {
 
             <View style={{flex: .5}} />
 
-            <View style={styles(theme).horizontal_view}>
-                <Text style={styles(theme).agree_text}>
-                    Hi
-                </Text>
-                <TouchableOpacity 
-                    onPress={changeButtonColor}
-                    style={{
-                        backgroundColor: buttonColor,
-                        borderColor: theme.colors.primary,
-                        borderWidth: 1,
-                        borderRadius: 5,
-                        justifyContent: 'center',
-                        alignContent: 'center',
-                        height: 30,
-                        width: 30, 
-                    }}
+            <AgreeBox
+                text={"Check this box to\nconfirm you are over 21"}
+                isButtonActive={isAgeButtonActive}
+                setIsButtonActive={setIsAgeButtonActive}
+            />
+
+            <View style={styles(theme).button_container }>
+                <Button 
+                    onPress={() => { /* TODO: Create Policy Screen */}}
+                    color={theme.colors.primary}
+                    title="Click to read Policy"
                 />
             </View>
 
@@ -69,49 +67,52 @@ const LegalScreen = ({navigation}, props) => {
 
 export default LegalScreen
 
-// const AgreeBox = (props) => {
-//     const theme = useTheme()
-//     const [buttonColor, setButtonColor] = useState(theme.colors.background)
-//     const [isButtonActive, setIsButtonActive] = useState(false);
+const AgreeBox = (props) => {
+    const theme = useTheme()
+    const [buttonColor, setButtonColor] = useState(theme.colors.background)
 
 
-//     const changeButtonColor = () => {
-//         if (isButtonActive) {
-//             setButtonColor(theme.colors.background)
-//             setIsButtonActive(false)
-//         } else {
-//             setButtonColor(theme.colors.primary)
-//             setIsButtonActive(true)
-//         }
-//     }
 
-//     return(
-//         <View style={styles(theme).horizontal_view}>
-//         <Text style={styles(theme).agree_text}>
-//             {props.text}
-//         </Text>
-//         <TouchableOpacity 
-//             onPress={changeButtonColor}
-//         >
-//             <View style={{
-//                 flex: 1,
-//                 backgroundColor: buttonColor,
-//                 borderColor: theme.colors.primary,
-//                 borderWidth: 1,
-//                 borderRadius: 5,
-//                 justifyContent: 'center',
-//                 alignContent: 'center',
-//             }} />
-//         </TouchableOpacity>
-//     </View>
-//     )
+    const changeButtonColor = () => {
+        if (props.isButtonActive) {
+            setButtonColor(theme.colors.background)
+            props.setIsButtonActive(false)
+        } else {
+            setButtonColor(theme.colors.primary)
+            props.setIsButtonActive(true)
+        }
+    }
+
+    return(
+        <View style={styles(theme).horizontal_view}>
+            <Text style={styles(theme).agree_text}>
+                {props.text}
+            </Text>
+            <TouchableOpacity 
+                onPress={changeButtonColor}
+                style={{
+                    // flex: 1,
+                    backgroundColor: buttonColor,
+                    borderColor: theme.colors.primary,
+                    borderWidth: 1,
+                    borderRadius: 5,
+                    // justifyContent: 'flex-start',
+                    // alignContent: 'center',
+                    height: 40,
+                    width: 40,
+
+                }}
+            />
+            
+        </View>
+    )
     
-// }
+}
 
 const styles = theme => (StyleSheet.create({
     background: {
         flex: 1,
-        justifyContent: 'center', 
+        justifyContent: 'space-evenly', 
         alignItems: 'center',
     },
     go_back_text: {
@@ -133,6 +134,8 @@ const styles = theme => (StyleSheet.create({
         textShadowOffset: {width: 2.5, height: 5},
         textShadowRadius: 5,
         padding: 10,
+        justifyContent: 'center',
+        alignContent: 'center',
 
     },
     buffer_1: {
@@ -142,15 +145,28 @@ const styles = theme => (StyleSheet.create({
         flex: 1,
     },
     horizontal_view: {
+        flex: 1,
         flexDirection: 'row',
-        justifyContent: 'center',
+        justifyContent: 'space-evenly',
         alignContent: 'center',
+        width: WIDTH,
     },
     agree_text: {
+        // flex: 1,
         color: theme.colors.text,
         fontFamily: theme.font.light,
-        fontSize: 20,
+        fontSize: 12,
         textAlign: 'center',
         paddingTop: 5,
-    }
+        // justifyContent: 'flex-start',
+        // alignContent: 'center',
+    },
+    button_container: {
+        backgroundColor: theme.colors.background,
+        borderColor: theme.colors.primary,
+        borderRadius: 5,
+        borderWidth: 1,
+        width: "70%",
+        //Todo: add shadows
+    },
 }));
