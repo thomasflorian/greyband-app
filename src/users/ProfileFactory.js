e9daf8edd7f234a1a22511d4e47b843c09f9d072
Cole Spector <cgs26@duke.edu>
2021-12-30 14:14:24 -0500

    made addEmail, addUsername and addPassword

--- src/users/ProfileFactory.js
+++ src/users/ProfileFactory.js
@@ -11,17 +11,16 @@ class ProfileFactory{
     }
 
 
-    addUsernamePass(username, password) {
+    addUsername(username) {
         if(this.newProfile != null) {
             if(this.#isViableUsername(username)){
                 this.newProfile.setUsername(username);
-                this.newProfile.setPassword(password);
                 return true;
             } else {
                 return false;
             }
         } else {
-            throw new Error('Must create profile before adding contents to it');
+            throw new Error('Must create profile before adding username to it');
         }
         
     }
@@ -41,13 +40,51 @@ class ProfileFactory{
         })
     }
 
+    addEmail(email) {
+        if(this.newProfile != null) {
+            if(this.#isViableEmail(email)){
+                this.newProfile.setEmail(email);
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            throw new Error('Must create profile before adding email to it');
+        }
+        
+    }
+
+    #isViableEmail(email){
+        var usersRef = db.collection("users");
+        const usernameQuery = usersRef.where("email", "==", email);
+        usernameQuery.get().then((querySnapshot) => {
+            if(querySnapshot.exists) {
+                return false
+            } else {
+                return true
+            }
+        }).catch((error) => {
+            console.log("Error getting username in usernameExists check: ", error);
+            return false
+        })
+    }
+
+    addPassword(password) {
+        if(this.newProfile != null) {
+            this.newProfile.setPassword(password);
+            return true;
+        } else {
+            throw new Error('Must create profile before adding password to it');
+        }
+    }
+
     addName(firstname, lastname) {
         if(this.newProfile != null) {
             this.newProfile.setFirstname(firstname);
             this.newProfile.setLastname(lastname);
             return true;
         } else {
-            throw new Error('Must create profile before adding contents to it');
+            throw new Error('Must create profile before adding name to it');
         }
         
     }
@@ -66,3 +103,5 @@ class ProfileFactory{
 
 
 }
+
+export default { ProfileFactory }
