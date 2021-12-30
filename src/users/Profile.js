05ee47a6d57df6e68d5bf3dbf5b0033ba3cc444b
Cole Spector <cgs26@duke.edu>
2021-12-30 13:41:15 -0500

    created setter methods

--- src/users/Profile.js
+++ src/users/Profile.js
@@ -18,7 +18,7 @@ class Profile {
         this.snapchat = ""
         this.linkedIn = ""
     }
-
+    //Getter Methods
     getUsername() {
         return this.username
     }
@@ -67,6 +67,59 @@ class Profile {
     getLinkedIn() {
         return this.linkedIn
     }
+
+
+    //Setter Methods
+    setUsername(username) {
+        this.username = username
+    }
+    setPassword(password) {
+        this.password = password
+    }
+    setFirstname(firstname) {
+        this.firstname = firstname
+    }
+    setLastname(lastname) {
+        this.lastname = lastname
+    }
+    setAge(age) {
+        this.age = age
+    }
+    setHometown(hometown) {
+        this.hometown = hometown
+    }
+    setWorkAndEducation(workAndEducation) {
+        this.workAndEducation = workAndEducation
+    }
+    setRelationshipStatus(relationshipStatus) {
+        this.relationshipStatus = relationshipStatus
+    }
+    setPronouns(pronouns) {
+        this.pronouns = pronouns
+    }
+    setSign(sign) {
+        this.sign = sign
+    }
+    setCurrentInterests(currentInterests) {
+        this.currentInterests = currentInterests
+    }
+    setBacHistory(bacHistory) {
+        this.bacHistory = bacHistory
+    }
+    setStarredProfiles(starredProfiles) {
+        this.starredProfiles = starredProfiles
+    }
+    setInstagram(instagram) {
+        this.instagram = instagram
+    }
+    setSnapchat(snapchat) {
+        this.snapchat = snapchat
+    }
+    setLinkedIn(linkedIn) {
+        this.linkedIn = linkedIn
+    }
+
+
 
 
 }
