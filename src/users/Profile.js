833479501ca7c49c73a459e372f4ce9016073e55
Cole Spector <cgs26@duke.edu>
2021-12-30 13:30:03 -0500

    basic profile setup

---
+++ src/users/Profile.js
@@ -0,0 +1,23 @@
+class Profile {
+    constructor(){
+        this.username = ""
+        this.password = ""
+        this.firstname = ""
+        this.lastname = ""
+        this.age = 0
+        this.hometown = ""
+        this.workAndEducation = ""
+        this.relationshipStatus = ""
+        this.pronouns = ""
+        this.sign = ""
+        this.currentInterests = []
+        this.bacHistory = []
+        this.starredProfiles = []
+        this.instagram = ""
+        this.snapchat = ""
+        this.linkedIn = ""
+    }
+
+}
+
+export default { Profile }
