ca6591aa4dd0b79ce903a7822f2897791f148307
Cole Spector <cgs26@duke.edu>
2021-12-30 14:01:55 -0500

    added addUsernamePass and addName methods

---
+++ src/users/ProfileFactory.js
@@ -0,0 +1,56 @@
+const { default: Profile } = require("./Profile");
+const { db } = require("../database/firebase-index");
+
+class ProfileFactory{
+    constructor() {
+        this.newProfile = null
+    }
+
+    startProfileCreation(){
+        this.newProfile = Profile()
+    }
+
+
+    addUsernamePass(username, password) {
+        if(this.newProfile != null) {
+            if(this.#isViableUsername(username)){
+                this.newProfile.setUsername(username);
+                this.newProfile.setPassword(password);
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            throw new Error('Must create profile before adding contents to it');
+        }
+        
+    }
+
+    addName(firstname, lastname) {
+        if(this.newProfile != null) {
+            this.newProfile.setFirstname(firstname);
+            this.newProfile.setLastname(lastname);
+            return true;
+        } else {
+            throw new Error('Must create profile before adding contents to it');
+        }
+        
+    }
+
+    #isViableUsername(username){
+        var usersRef = db.collection("users");
+        const usernameQuery = usersRef.where("username", "==", username);
+        usernameQuery.get().then((querySnapshot) => {
+            if(querySnapshot.exists) {
+                return false
+            } else {
+                return true
+            }
+        }).catch((error) => {
+            console.log("Error getting username in usernameExists check: ", error);
+            return false
+        })
+    }
+
+
+}
