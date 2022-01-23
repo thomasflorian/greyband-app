5af02cf06e3f672e9c5c5f420de391dfd085ea28
Cole Spector <cgs26@duke.edu>
2022-01-22 22:02:22 -0500

    added BaseInfo to Stack

--- App.js
+++ App.js
@@ -19,6 +19,7 @@ import CreateAccountScreen from './screens/login/CreateAccountScreen';
 import LegalScreen from './screens/login/LegalScreen';
 import Toast, {ErrorToast} from 'react-native-toast-message';
 import {Icon} from 'react-native-elements';
+import BaseInfo from './screens/login/BaseInfo';
 
 export default function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -109,6 +110,7 @@ const toastConfig = {
               <LoggedOutStack.Screen name="Login" component={LoginScreen} />
               <LoggedOutStack.Screen name="CreateAccount" component={CreateAccountScreen} />
               <LoggedOutStack.Screen name="Legal" component={LegalScreen} />
+              <LoggedOutStack.Screen name="Base" component={BaseInfo} />
             </LoggedOutStack.Navigator>
             <Toast config={toastConfig}/>
             </>
